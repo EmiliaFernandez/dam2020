ff236abb34d029291f0b5de1928abb6c6d235546
Emilia <Emilia@LAPTOP-K2OUQM69>
2020-02-12 23:24:33 +0100

    Primera versión

---
+++ src/dam/DAM.java
@@ -0,0 +1,19 @@
+package dam;
+
+
+
+
+/**
+ *@version 1.0
+ *@author Emilia
+ */
+public class DAM {
+
+    /**
+     * @param args No hay parámetros
+     */
+    public static void main(String[] args) {
+        System.out.println("hola");
+    }
+
+}
